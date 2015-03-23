e5be67c70302b02078c25c21820a0ab09e16b6ea
David Qorashi <davoud.qorashi@gmail.com>
2015-03-23 15:07:38 -0400

    Integrated into view controller

--- LeapGestureRecognition/ViewController.h
+++ LeapGestureRecognition/ViewController.h
@@ -9,6 +9,7 @@
 #import <Cocoa/Cocoa.h>
 
 @interface ViewController : NSViewController
+@property (weak) IBOutlet NSTextField *testLabel;
 
 @end
 
--- LeapGestureRecognition/ViewController.m
+++ LeapGestureRecognition/ViewController.m
@@ -7,27 +7,62 @@
 //
 
 #import "ViewController.h"
-#import "MaslLeapController.h"
+#import "LeapObjectiveC.h"
 
 @implementation ViewController
-{
-    MaslLeapController *controller;
-}
+
 
 
 - (void)viewDidLoad {
     [super viewDidLoad];
 
     // Do any additional setup after loading the view.
-    controller = [[MaslLeapController alloc] init];
-    [controller run];
-
+//    self.testLabel.stringValue = @"Hello World";
+    
+    LeapController *controller = [[LeapController alloc] init];
+    [controller addListener:self];
+    
 }
 
 - (void)setRepresentedObject:(id)representedObject {
     [super setRepresentedObject:representedObject];
 
     // Update the view, if already loaded.
+}
+
+#pragma mark - Leap Controller Callbacks
+
+- (void)onInit:(NSNotification *)notification
+{
+    NSLog(@"Initialized");
+}
+
+- (void)onConnect:(NSNotification *)notification
+{
+    NSLog(@"Connected");
+    LeapController *aController = (LeapController *)[notification object];
+    [aController enableGesture:LEAP_GESTURE_TYPE_CIRCLE enable:YES];
+    [aController enableGesture:LEAP_GESTURE_TYPE_KEY_TAP enable:YES];
+    [aController enableGesture:LEAP_GESTURE_TYPE_SCREEN_TAP enable:YES];
+    [aController enableGesture:LEAP_GESTURE_TYPE_SWIPE enable:YES];
+}
+
+- (void)onDisconnect:(NSNotification *)notification
+{
+    //Note: not dispatched when running in a debugger.
+    NSLog(@"Disconnected");
+}
+
+- (void)onFrame:(NSNotification *)notification
+{
+    LeapController *aController = (LeapController *)[notification object];
+    
+    LeapFrame *frame = [aController frame:0];
+    NSLog(@"Frame id: %lld, timestamp: %lld, hands: %ld, fingers: %ld, tools: %ld, gestures: %ld",
+          [frame id], [frame timestamp], [[frame hands] count],
+          [[frame fingers] count], [[frame tools] count], [[frame gestures:nil] count]);
+    
+    self.testLabel.stringValue = [[NSString alloc] initWithFormat:@"%lld", [frame id]];
 }
 
 @end
